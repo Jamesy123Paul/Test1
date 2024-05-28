3fe0c01aac5002855c6b8544782be75291615881
Jamesy123Paul <ojieh.p78@mail.com>
2024-05-28 08:47:23 +0100

    Added Paul to the data

--- Test1/Controllers/WeatherForecastController.cs
+++ Test1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Test1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Paul"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
